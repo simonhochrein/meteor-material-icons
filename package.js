c34d5cc78b3bc9be1c1495dc426cdc78c2368fe9
simonhochrein <simon@ministrycrm.com>
2016-07-26 15:47:52 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'simonhochrein:material-icons',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'A Meteor wrapper for Google\'s Material Icons',
   // URL to the Git repository containing the source code for this package.
